81750c6775d3996fa68de775bb35870d34a2c011
rrokkam <rohithrokkam@gmail.com>
2020-05-15 12:12:16 -0700

    Use external hex library to convert from literal

--- src/xor.rs
+++ src/xor.rs
@@ -13,15 +13,15 @@ fn fixed_xor(first: Vec<u8>, second: Vec<u8>) -> Vec<u8> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::bytes::Hex;
+    use hex::FromHex;
 
     #[test]
     fn test_fixed_xor() {
-        let first = Vec::from(Hex::new("1c0111001f010100061a024b53535009181c".to_string()));
-        let second = Vec::from(Hex::new("686974207468652062756c6c277320657965".to_string()));
+        let first = Vec::from_hex("1c0111001f010100061a024b53535009181c").unwrap();
+        let second = Vec::from_hex("686974207468652062756c6c277320657965").unwrap();
         assert_eq!(
-            Hex::from(fixed_xor(first, second)),
-            Hex::new("746865206b696420646f6e277420706c6179".to_string())
+            fixed_xor(first, second),
+            Vec::from_hex("746865206b696420646f6e277420706c6179").unwrap()
         );
     }
 }
