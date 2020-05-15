f3d6de04f2284ef822555cd9b180c278c5a9a57d
rrokkam <rohithrokkam@gmail.com>
2020-05-14 23:46:20 -0700

    Add stub and tests for Hex<->Vec<u8> conversions

--- src/bytes.rs
+++ src/bytes.rs
@@ -1,7 +1,30 @@
+pub struct Hex(String);
+
+impl From<Vec<u8>> for Hex {
+    fn from(bytes: Vec<u8>) -> Self {
+        Hex("".to_string())
+    }
+}
+
+impl From<Hex> for Vec<u8> {
+    fn from(hex: Hex) -> Self {
+        Vec::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn hex_from_vec() {
+        let hex = Hex::from(vec![99, 111]);
+        assert_eq!(hex.0, "636f");
+    }
+
+    #[test]
+    fn vec_from_hex() {
+        let vec = Vec::from(Hex("636f".to_string()));
+        assert_eq!(vec, vec![99, 111]);
     }
 }
