34131d776f8098d9f402c1e82fa786a4ed10d6df
rrokkam <rohithrokkam@gmail.com>
2020-05-15 20:29:13 -0700

    Check length with Deref<Target = [u8]>

--- src/xor.rs
+++ src/xor.rs
@@ -1,14 +1,15 @@
 use std::iter::FromIterator;
+use std::ops::Deref;
 
 #[allow(dead_code)]
 fn fixed_xor<T, U>(first: T, second: T) -> U
 where
-    T: IntoIterator<Item = u8>,
+    T: IntoIterator<Item = u8> + Deref<Target = [u8]>,
     U: FromIterator<u8>,
 {
-    //    if first.len() != second.len() {
-    //        panic!("Can't XOR two bytestrings of different length");
-    //    }
+    if first.len() != second.len() {
+        panic!("Can't XOR two bytestrings of different length");
+    }
     first
         .into_iter()
         .zip(second.into_iter())
@@ -26,6 +27,6 @@ mod tests {
         let first = hex!("1c0111001f010100061a024b53535009181c").to_vec();
         let second = hex!("686974207468652062756c6c277320657965").to_vec();
         let xored: Vec<u8> = fixed_xor(first, second);
-        assert_eq!(xored, hex!("746865206b696420646f6e277420706c6179"));
+        assert_eq!(xored, hex!("746865206b696420646f6e277420706c6179").to_vec());
     }
 }
