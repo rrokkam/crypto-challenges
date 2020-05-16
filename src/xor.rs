25be3539119d56340fb027cf2d78d2dd098ad49e
rrokkam <rohithrokkam@gmail.com>
2020-05-16 11:01:01 -0700

    Simplify and implement char_frequencies and score

--- src/xor.rs
+++ src/xor.rs
@@ -1,20 +1,31 @@
-use std::iter::FromIterator;
-use std::ops::Deref;
+use std::collections::HashMap;
 
-#[allow(dead_code)]
-fn fixed_xor<T, U>(first: T, second: T) -> U
-where
-    T: IntoIterator<Item = u8> + Deref<Target = [u8]>,
-    U: FromIterator<u8>,
-{
-    if first.len() != second.len() {
-        panic!("Can't XOR two bytestrings of different length");
-    }
+fn fixed_xor(first: Vec<u8>, second: Vec<u8>) -> Vec<u8> {
     first
-        .into_iter()
-        .zip(second.into_iter())
+        .iter()
+        .zip(second.iter())
         .map(|(a, b)| a ^ b)
         .collect()
+}
+
+fn char_frequencies(corpus: String) -> HashMap<char, f64> {
+    corpus
+        .chars()
+        .fold(HashMap::new(), |mut acc, c| {
+            *acc.entry(c).or_insert(0) += 1;
+            acc
+        })
+        .into_iter()
+        .map(|(c, count)| (c, count as f64 / corpus.len() as f64))
+        .collect()
+}
+
+fn score(text: String, freqs: HashMap<char, f64>) -> f64 {
+    let mut score = 0.0;
+    for c in text.chars() {
+        score += freqs.get(&c).unwrap_or(&0.0);
+    }
+    score
 }
 
 #[cfg(test)]
@@ -26,7 +37,7 @@ mod tests {
     fn test_fixed_xor() {
         let first = hex!("1c0111001f010100061a024b53535009181c").to_vec();
         let second = hex!("686974207468652062756c6c277320657965").to_vec();
-        let xored: Vec<u8> = fixed_xor(first, second);
+        let xored = fixed_xor(first, second);
         assert_eq!(xored, hex!("746865206b696420646f6e277420706c6179").to_vec());
     }
 }
